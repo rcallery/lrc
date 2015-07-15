62a8ea598d3a85d9d7db1a9dd39178e2c00b70bc
Julia Watzek <j.watzek@gmail.com>
2015-07-15 19:22:04 -0400

    moving cursor and keeping it on-screen with Rect functions

--- joystick_test/joystick_wohoo.py
+++ joystick_test/joystick_wohoo.py
@@ -11,8 +11,6 @@ def pellet():
 
 
 pygame.init()
-
-# for fullscreen, add:  (pygame.NOFRAME and pygame.FULLSCREEN)
 screen = pygame.display.set_mode((800, 450))
 
 # initialize joystick
@@ -22,46 +20,37 @@ joy.init()
 # set cursor speed
 cursorSpeed = 5
 
-# set start position
-x = 385
-y = 210
-
 # load images
 crosshairs = pygame.image.load("crosshairs.png")
 background = pygame.image.load("background.jpg")
 
 # set rectangles for images
-Cursor = pygame.Rect(x, y, 30, 30)
-Back = pygame.Rect(0, 0, 800, 450)
+cursor = pygame.Rect(385, 210, 30, 30)
+scrRect = pygame.Rect(0, 0, 800, 450)
 
 clock = pygame.time.Clock()
 
 
 while True:
+    # quit on QUIT, [Esc], and [Q]
     for event in pygame.event.get():
         if event.type == QUIT or (event.type == KEYDOWN and (event.key == K_ESCAPE or event.key == K_q)):
             pygame.quit()
             sys.exit()
 
+    # move cursor
     h_axis_pos = round(joy.get_axis(0), 5)
     v_axis_pos = round(joy.get_axis(1), 5)
-    
-    # update position
-    x += int(h_axis_pos * cursorSpeed)
-    y += int(v_axis_pos * cursorSpeed)
+    cursor.move_ip(h_axis_pos * cursorSpeed, 
+                   v_axis_pos * cursorSpeed)
 
-    Cursor.left = x
-    Cursor.top = y
-
-    # keep cursor from going off-screen
-    if x < 0:       x = 0
-    elif x > 770:   x = 770
-    if y < 0:       y = 0
-    elif y > 420:   y = 420
+    # keep cursor on-screen
+    cursor.clamp_ip(scrRect)
 
     # update screen
-    screen.blit(background, Back)
-    screen.blit(crosshairs, Cursor)
-
+    screen.blit(background, scrRect)
+    screen.blit(crosshairs, cursor)
     pygame.display.update()
+
+    # frame rate
     clock.tick(60)
