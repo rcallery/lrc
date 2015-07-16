7f7f860dd4d4c7e79189a822d4fb1c27a90862b6
Julia Watzek <j.watzek@gmail.com>
2015-07-16 12:23:48 -0400

    initial commit

---
+++ lrc.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pygame, sys, os, time
+from pygame.locals import *
+
+# helper functions
+def pellet(num = 1):
+    '''Dispense [num] pellets. Defaults to 1.'''
+    for i in range(num):
+        # subprocess.call("c:/pellet.exe")
+        os.system("c:/pellet.exe")
+
+def sound(boolCorr):
+    '''If True, play 'correct' sound; if False, play 'incorrect' sound.'''
+    if boolCorr:    pygame.mixer.music.load('sounds/correct.wav')
+    else:           pygame.mixer.music.load('sounds/incorrect.wav')
+
+    pygame.mixer.music.play()
+
+def quitEscQ():
+    '''Quit pygame on QUIT, [Esc], and [Q].'''
+    for event in pygame.event.get():
+        if event.type == QUIT or (event.type == KEYDOWN and (event.key == K_ESCAPE or event.key == K_q)):
+            pygame.quit()
+            sys.exit()
+
+pygame.init()
+pygame.mixer.init()
+
+screen = pygame.display.set_mode((800, 600), (pygame.NOFRAME and pygame.FULLSCREEN))
+scrRect = pygame.Rect(0, 0, 800, 600)
+
+# hide mouse and initialize joystick
+pygame.mouse.set_visible(False)
+joy = pygame.joystick.Joystick(0)
+joy.init()
