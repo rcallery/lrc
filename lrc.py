1db44a3a4a8b7d90c0f5d1a7b8e762dbc280b8ef
Julia Watzek <j.watzek@gmail.com>
2015-07-17 09:55:24 -0400

    preload sounds, removed redundant pygame.mixer.init()

--- lrc.py
+++ lrc.py
@@ -2,19 +2,23 @@ import pygame, sys, os
 import math, random, time, datetime
 from pygame.locals import *
 
+pygame.init()
+
+# load sounds
+sndCor = pygame.mixer.Sound('sounds/correct.wav')
+sndIncor = pygame.mixer.Sound('sounds/incorrect.wav')
+
 # helper functions
+def sound(boolCorr):
+    '''If True, play 'correct' sound; if False, play 'incorrect' sound.'''
+    if boolCorr:    sndCor.play()
+    else:           sndIncor.play()
+
 def pellet(num = 1):
     '''Dispense [num] pellets. Defaults to 1.'''
     for i in range(num):
         # subprocess.call('c:/pellet.exe')
         os.system('c:/pellet.exe')
-
-def sound(boolCorr):
-    '''If True, play 'correct' sound; if False, play 'incorrect' sound.'''
-    if boolCorr:    pygame.mixer.music.load('sounds/correct.wav')
-    else:           pygame.mixer.music.load('sounds/incorrect.wav')
-
-    pygame.mixer.music.play()
 
 def quitEscQ():
     '''Quit pygame on QUIT, [Esc], and [Q].'''
@@ -22,9 +26,6 @@ def quitEscQ():
         if event.type == QUIT or (event.type == KEYDOWN and (event.key == K_ESCAPE or event.key == K_q)):
             pygame.quit()
             sys.exit()
-
-pygame.init()
-pygame.mixer.init()
 
 screen = pygame.display.set_mode((800, 600), (pygame.NOFRAME and pygame.FULLSCREEN))
 scrRect = pygame.Rect(0, 0, 800, 600)
