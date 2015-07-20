e62760649e5877d649aad68e1f866bc56abefea1
Julia Watzek <j.watzek@gmail.com>
2015-07-20 14:30:40 -0400

    added sprite class Box with defaults for cursor

--- lrc.py
+++ lrc.py
@@ -5,12 +5,41 @@ from pygame.locals import *
 pygame.init()
 
 # load sounds
+# [dev] local copies
 # sndCor = pygame.mixer.Sound('c:/correct.wav')
 # sndIncor = pygame.mixer.Sound('c:/incorrect.wav')
-# [dev] local copies
 sndCor = pygame.mixer.Sound('../correct.wav')
 sndIncor = pygame.mixer.Sound('../incorrect.wav')
 pelletPath = 'c:/pellet.exe'
+
+# helper class
+
+class Box(pygame.sprite.Sprite):
+    """Class for box with default dimensions (20, 20), in the screen 
+       center, black colour, and speed 8."""
+
+    def __init__(self, size = (20, 20), pos = (400, 300), col = (0, 0, 0), speed = 8):
+        super(Box, self).__init__()
+        self.image = pygame.Surface(size)
+        self.image.fill(col)
+        self.rect = self.image.get_rect()
+        self.pos = self.rect.center = pos
+        self.speed = 8
+
+    def move(self, x, y):
+        """Move box x pixels to the right and y pixels down. Keep box on-screen."""
+        self.rect.move_ip(x, y)
+        self.rect.clamp_ip(scrRect)
+
+    def mv2pos(self, pos):
+        """Move box to position (x, y)."""
+        self.rect.center = pos
+
+    def collide(self, list):
+        """Test if box collides with a rectangle in the list, returns index. 
+           Returns -1 when no collision is occuring."""
+        return self.rect.collidelist(list)
+
 
 # helper functions
 def sound(boolCorr):
@@ -19,14 +48,15 @@ def sound(boolCorr):
     else:           sndIncor.play()
 
 def pellet(num = 1):
-    '''Dispense [num] pellets. Defaults to 1.'''
+    '''Dispense [num] pellets, then wait 700 ms. Defaults to 1.
+       Doesn't do anything if it can't find `pellet.exe` (for development).'''
     if os.path.isfile(pelletPath):
         for i in range(num):
             os.system(pelletPath)
             pygame.time.delay(700)
 
 def quitEscQ():
-    '''Quit pygame on QUIT, [Esc], and [Q].'''
+    '''Quit pygame on QUIT, [Esc], and [Q]. Use inside main game loop.'''
     for event in pygame.event.get():
         if event.type == QUIT or (event.type == KEYDOWN and (event.key in (K_ESCAPE, K_q))):
             pygame.quit()
@@ -45,9 +75,3 @@ joyCount = pygame.joystick.get_count()
 if joyCount > 0:
     joy = pygame.joystick.Joystick(0)
     joy.init()
-
-# set cursor speed, size, colour, and start position
-cursorSpeed = 8
-cursorSize = (20, 20)
-cursorCol = (0, 0, 0)
-cursorPos = (400, 200)
